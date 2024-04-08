074ef8ae8bac7d50a5e371eff5bdedb1d47c872e
jennyo88 <39907844+jennyo88@users.noreply.github.com>
2024-04-08 13:28:14 -0700

    Update compare_outputs.sh

--- scripts/compare_outputs.sh
+++ scripts/compare_outputs.sh
@@ -4,23 +4,23 @@
 
 # Check if the number of arguments is not equal to 2
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <output1> <output2>"
+    echo "Usage: $0 <ping_output> <curl_output>"
     exit 1
 fi
 
 # Assign arguments to variables
-output1="$1"
-output2="$2"
+ping_output="$1"
+curl_output="$2"
 
 # Compare outputs line by line
-IFS=$'\n' read -r -a lines1 <<< "$output1"
-IFS=$'\n' read -r -a lines2 <<< "$output2"
+IFS=$'\n' read -r -a lines1 <<< "$ping_output"
+IFS=$'\n' read -r -a lines2 <<< "$curl_output"
 
 for i in "${!lines1[@]}"; do
     if [ "${lines1[$i]}" != "${lines2[$i]}" ]; then
         echo "The outputs differ at line $((i+1)):"
-        echo "Output 1: ${lines1[$i]}"
-        echo "Output 2: ${lines2[$i]}"
+        echo "Ping Output: ${lines1[$i]}"
+        echo "Curl Output: ${lines2[$i]}"
         exit 0
     fi
 done
