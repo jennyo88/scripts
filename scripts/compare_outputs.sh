684c750a2d9b7cf3a89710515890d1c66e4304c6
jennyo88 <39907844+jennyo88@users.noreply.github.com>
2024-04-08 15:13:38 -0700

    Update compare_outputs.sh

--- scripts/compare_outputs.sh
+++ scripts/compare_outputs.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/env bash
 
-# Bash script to compare the IP address obtained from ping output with the IP address obtained from curl output.
+## SAMPLE
+## ./compare_outputs.sh -p sample.com -c sample.com
+## ./compare_outputs.sh --ping sample.com --curl sample.com
 
-##
-## ./compare_outputs.sh "$(ping -c 1 sample.com)" "$(curl -s sample.com)"
+# Default values for flags
+ping_url=""
+curl_url=""
 
-# Check if the number of arguments is not equal to 2
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <ping_output> <curl_output>"
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -p|--ping)
+            ping_url="$2"
+            shift
+            shift
+            ;;
+        -c|--curl)
+            curl_url="$2"
+            shift
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Check if both ping and curl URLs are provided
+if [ -z "$ping_url" ] || [ -z "$curl_url" ]; then
+    echo "Usage: $0 -p <ping_url> -c <curl_url>"
     exit 1
 fi
 
@@ -21,14 +45,12 @@ get_ip_from_curl() {
     echo "$1"
 }
 
-# Assign arguments to variables
-ping_output="$1"
-curl_output="$2"
-
-# Get IP address from ping output
+# Get IP address from ping command
+ping_output=$(ping -c 1 "$ping_url")
 ping_ip=$(get_ip_from_ping "$ping_output")
 
-# Get IP address from curl output
+# Get IP address from curl command
+curl_output=$(curl -s "$curl_url")
 curl_ip=$(get_ip_from_curl "$curl_output")
 
 # Compare IP addresses (ignoring trailing characters such as colons)
